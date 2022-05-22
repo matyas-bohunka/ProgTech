88fc0b2cfc2d84ad807b6642848f1f2216717ec0
Horváth Márton <horvathmarton02@gmail.com>
2022-05-22 13:26:34 +0200

    Added Memory class to LaptopParts

---
+++ ProgTech/src/main/java/Classes/LaptopParts/Memory.java
@@ -0,0 +1,49 @@
+package Classes.LaptopParts;
+
+public class Memory {
+    private int id;
+    private String name;
+    private String type;
+    private int capacity;
+    private int speed;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
