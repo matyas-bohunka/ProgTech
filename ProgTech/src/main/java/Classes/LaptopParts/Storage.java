3f9d86cc9963c426dc7b937f7fe247abcfb798f1
Horváth Márton <horvathmarton02@gmail.com>
2022-05-22 13:52:00 +0200

    Added Storage class to LaptopParts

---
+++ ProgTech/src/main/java/Classes/LaptopParts/Storage.java
@@ -0,0 +1,58 @@
+package Classes.LaptopParts;
+
+public class Storage {
+    private int id;
+    private String name;
+    private String type;
+    private int speed;
+    private int capacity;
+    private int price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int Price) {
+        this.price = price;
+    }
+}
