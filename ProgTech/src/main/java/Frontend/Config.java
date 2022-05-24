a3bd264f1e9c4d2293b8751a0620fbf4477e6ef1
Horváth Márton <horvathmarton02@gmail.com>
2022-05-24 20:39:54 +0200

    Added missing extendsion to config

--- ProgTech/src/main/java/Frontend/Config.java
+++ ProgTech/src/main/java/Frontend/Config.java
@@ -12,7 +12,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
-public class Config {
+public class Config extends JFrame{
     private JPanel ConfPanel;
     private JPanel TypePanel;
     private JButton legionButton;
