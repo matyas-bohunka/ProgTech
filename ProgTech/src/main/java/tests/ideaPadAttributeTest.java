bfaff16d13e01cc0deaa072bbba9a290b6ae146f
Horváth Márton <horvathmarton02@gmail.com>
2022-05-25 12:19:20 +0200

    Added login test

--- ProgTech/src/main/java/tests/ideaPadAttributeTest.java
+++ ProgTech/src/main/java/tests/ideaPadAttributeTest.java
@@ -1,7 +1,6 @@
 package tests;
 
 import Classes.Laptop_types.IdeaPad;
-import Classes.Laptop_types.ThinkPad;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
