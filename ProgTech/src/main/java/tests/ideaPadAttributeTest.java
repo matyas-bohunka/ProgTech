724f1df52edc1f1c89dd08b2740112b969aa76e9
matyas-bohunka <matyibohunka@gmail.com>
2022-05-25 10:59:00 +0200

    Fixed tests

--- ProgTech/src/main/java/tests/ideaPadAttributeTest.java
+++ ProgTech/src/main/java/tests/ideaPadAttributeTest.java
@@ -1,2 +1,27 @@
-package tests;public class ideaPadAttributeTest {
+package tests;
+
+import Classes.Laptop_types.IdeaPad;
+import Classes.Laptop_types.ThinkPad;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+public class ideaPadAttributeTest {
+    @Mock
+    IdeaPad ideaPad = new IdeaPad(100000);
+
+    @Test
+    public void ideaPadPriceTest() {
+        int expectedPrice = 100000;
+        int actualPrice = ideaPad.getPrice();
+        Assertions.assertEquals(expectedPrice, actualPrice);
+
+    }
+
+    @Test
+    public void ideaPadTypeTest() {
+        String expectedType = "ideapad";
+        String actualType = ideaPad.getType();
+        Assertions.assertEquals(expectedType, actualType);
+    }
 }
--- ProgTech/src/main/java/tests/thinkPadAttributeTest.java
+++ ProgTech/src/main/java/tests/thinkPadAttributeTest.java
@@ -1,2 +1,26 @@
-package tests;public class thinkPadAttributeTest {
+package tests;
+
+import Classes.Laptop_types.ThinkPad;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+public class thinkPadAttributeTest {
+    @Mock
+    ThinkPad thinkPad = new ThinkPad(100000);
+
+    @Test
+    public void thinkPadPriceTest() {
+        int expectedPrice = 110000;
+        int actualPrice = thinkPad.getPrice();
+        Assertions.assertEquals(expectedPrice, actualPrice);
+
+    }
+
+    @Test
+    public void thinPadTypeTest() {
+        String expectedType = "thinkpad";
+        String actualType = thinkPad.getType();
+        Assertions.assertEquals(expectedType, actualType);
+    }
 }
